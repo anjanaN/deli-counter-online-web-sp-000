cf667c6a3f31275005dd31aa1ea2886cd6483832
Anjana Nair <anju740@gmail.com>
2019-04-27 17:12:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   if array.length > 0
-    puts "The line is currently: "
+    greeting = "The line is currently: "
     array.each do |person|
       puts "#{array.index(person)+1}. #{person}"
     end
