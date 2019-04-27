05b5421da501fca442451bcc697bdbbd7aeb8307
Anjana Nair <anju740@gmail.com>
2019-04-27 17:20:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ katz_deli = []
 
 def line(array)
   if array.length > 0
-    greeting = "The line is currently: "
+    greeting = "The line is currently:"
     array.each do |person|
-      greeting << "#{array.index(person)+1}. "
+      greeting << " #{array.index(person)+1}. "
       greeting << "#{person}"
     end
   end
